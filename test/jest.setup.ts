4a7133d927096fb321ed708a220bae6e6d3ed43c
YashPShah2001 <yashpshah8@gmail.com>
2025-08-20 21:21:31 +0100

    added resizeobserver

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,1 +1,9 @@
 import "@testing-library/jest-dom";
+// ResieObserver was required for rechart elements as jsdom does not implement ResizeObserver.
+class ResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  }
+  
+  (global as any).ResizeObserver = ResizeObserver;
