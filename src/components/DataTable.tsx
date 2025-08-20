0da171b865883db38d8fc6e800e0b4ede3cb3a43
YashPShah2001 <yashpshah8@gmail.com>
2025-08-20 14:47:04 +0100

    added borders and handelled hover

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -6,21 +6,21 @@ interface props{
 
 export default function DataTable({data}:props){
     return (
-        <div className="overflow-auto my-4 bg-slate-800 ">
-            <table className="w-full table-fixed shadow-md rounded-lg divide-y divide-slate-950 ">
-                <thead className="bg-slate-700">
-                    <tr>
-                        <th className="font-semibold p-4 text-slate-200 text-left">Date</th>
-                        <th className="font-semibold p-4 text-slate-200 text-left">Calories Burned</th>
-                        <th className="font-semibold p-4 text-slate-200 text-left">Steps</th>
+        <div className="overflow-y-auto bg-slate-800 md:rounded-xl border border-slate-50">
+            <table className="w-full table-fixed divide-y divide-slate-50 ">
+                <thead className="bg-slate-700 sticky top-0 z-10">
+                    <tr className="text-slate-200">
+                        <th className="font-semibold p-4 text-left">Date</th>
+                        <th className="font-semibold p-4 text-left">Calories Burnt</th>
+                        <th className="font-semibold p-4 text-left">Steps</th>
                     </tr>
                 </thead>
-                <tbody className="divide-y divide-slate-950 ">
+                <tbody className="divide-y divide-slate-500 ">
                     {data.map((e) => (
-                        <tr key={e.date} className="hover:bg-gray-50">
-                            <td className="p-4 text-sm  text-slate-400">{e.date}</td>
-                            <td className="p-4 text-sm text-slate-400">{e.calories}</td>
-                            <td className="p-4 text-sm text-slate-400">{e.steps}</td>
+                        <tr key={e.date} className="hover:bg-gray-50 hover:text-slate-800 text-slate-400">
+                            <td className="p-4 text-sm">{e.date}</td>
+                            <td className="p-4 text-sm">{e.calories}</td>
+                            <td className="p-4 text-sm">{e.steps}</td>
                         </tr>
                     ))}
                 </tbody>
