6a331bbd408eede1667a74387f2e340e0d2138ef
YashPShah2001 <yashpshah8@gmail.com>
2025-08-21 00:28:58 +0100

    applied gradient style to header

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,10 @@ function App() {
 
   return (
     <>
-      <div className='bg-gray-300 dark:bg-gray-700 p-4 w-full shadow-md shadow-slate-500'>
-        <h1 className='text-2xl font-bold text-slate-800 dark:text-slate-300'>Weekly exercise summary</h1>
+      <div className="p-4 w-full 
+                      bg-gradient-to-r dark:bg-gradient-to-l from-slate-300 via-slate-500 to-slate-700
+                      shadow-md shadow-slate-500">
+        <h1 className="text-2xl font-bold text-slate-800 dark:text-slate-300">Weekly exercise summary</h1>
       </div>
       <div className="flex flex-col md:flex-row gap-4 p-3">
         <div className="h-auto flex-1">
